fecdb68141ea922fcc2cb2fa60fa08e4ae9cdfcf
ToropkinEa <e.toropkin@rdsystems.ru>
2023-02-05 14:48:18 +0300

    сommit new branch

--- src/by/teachmeskills/homeworks/hw_10022023/HWLesson3Class.java
+++ src/by/teachmeskills/homeworks/hw_10022023/HWLesson3Class.java
@@ -3,15 +3,15 @@ package by.teachmeskills.homeworks.hw_10022023;
 public class HWLesson3Class
 {
     public static void main(String[] args)
-    {   int nVar1 = 1;
+    {   int nVar1 = 3;
         int nVar2 = 2;
 
-        int nSumma = nVar1 + nVar2;
+        int nSumma = nVar1 - nVar2;
 
         System.out.println("nVar1 = " + nVar1);
         System.out.println("nVar2 = " + nVar2);
 
-        System.out.println("Сумма = " + nSumma);
+        System.out.println("Разница = " + nSumma);
     }
 
 }
