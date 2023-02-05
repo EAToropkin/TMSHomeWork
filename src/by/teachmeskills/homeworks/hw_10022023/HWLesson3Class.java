b77c797f6b424c97a0aaa98d9db3b59554bfb20e
ToropkinEa <e.toropkin@rdsystems.ru>
2023-02-05 14:41:13 +0300

    test commit

--- src/by/teachmeskills/homeworks/hw_10022023/HWLesson3Class.java
+++ src/by/teachmeskills/homeworks/hw_10022023/HWLesson3Class.java
@@ -1,13 +1,16 @@
 package by.teachmeskills.homeworks.hw_10022023;
 
-import java.util.Arrays;
-
 public class HWLesson3Class
 {
     public static void main(String[] args)
     {   int nVar1 = 1;
         int nVar2 = 2;
+
         int nSumma = nVar1 + nVar2;
+
+        System.out.println("nVar1 = " + nVar1);
+        System.out.println("nVar2 = " + nVar2);
+
         System.out.println("Сумма = " + nSumma);
     }
 
