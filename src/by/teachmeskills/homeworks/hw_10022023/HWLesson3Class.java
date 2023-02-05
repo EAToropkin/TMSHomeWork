3b5278e01580ec13e06529e27b7e28e951d45a88
ToropkinEa <e.toropkin@rdsystems.ru>
2023-02-05 14:19:45 +0300

    Initial

---
+++ src/by/teachmeskills/homeworks/hw_10022023/HWLesson3Class.java
@@ -0,0 +1,14 @@
+package by.teachmeskills.homeworks.hw_10022023;
+
+import java.util.Arrays;
+
+public class HWLesson3Class
+{
+    public static void main(String[] args)
+    {   int nVar1 = 1;
+        int nVar2 = 2;
+        int nSumma = nVar1 + nVar2;
+        System.out.println("Сумма = " + nSumma);
+    }
+
+}
